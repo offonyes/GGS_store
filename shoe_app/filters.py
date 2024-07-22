1c6682e8d47ae370a0d21938e0bc39ce60e53833
offonyes <dimkakatran@gmail.com>
2024-07-22 18:53:06 +0400

    first commit

---
+++ shoe_app/filters.py
@@ -0,0 +1,21 @@
+from admin_auto_filters.filters import AutocompleteFilter
+
+
+class AuthorsFilter(AutocompleteFilter):
+    title = 'Author'
+    field_name = 'authors'
+
+
+class GenresFilter(AutocompleteFilter):
+    title = 'Genre'
+    field_name = 'genres'
+
+
+class BooksFilter(AutocompleteFilter):
+    title = 'Book'
+    field_name = 'book'
+
+
+class BorrowersFilter(AutocompleteFilter):
+    title = 'Borrower'
+    field_name = 'borrower'
